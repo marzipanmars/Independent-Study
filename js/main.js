80f387d7ad4b1592cc585e14d97f437d942d19d3
marzipanmars <malam@vassar.edu>
2017-03-24 04:50:56 -0700

    scale different JSON properties

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function visualizeData() {
   /* set width and height of svg image */
   var canvas_width = 1000,
-      canvas_height = 800;
+      canvas_height = 700;
 
   /* scaling to adjust height of scaleBars if they exceed the range */
   var scaling = d3.scaleLinear()
@@ -15,18 +15,23 @@ function visualizeData() {
     .append("g")
       .attr("transform", "translate(90, 90)")
 
-/* set scaleBar labels, width, height, fill-color, and y-offset */
+  /* set scaleBar labels, width, height, fill-color, and y-offset */
   var data_categories = ["expression", "awareness", "hope", "embarrassment", "empathy", "fear"], //"hunger", "joy", "memory", "morality", "joy", "pain", "personality", "attainment", "pleasure", "pride", "anger", "self-restraint", "thought"],
       scale_width = 500,
       scale_height = 25,
       fill_color = "Gainsboro"
-      dy = 120;
+      dy = 100;
 
-/* create as many rectangles as there are elements in "data_categories" */
+  /* create as many rectangles as there are elements in "data_categories" */
   var scaleBars = canvas.selectAll("rect")
     /* binding data */
     .data(data_categories)
     .enter()
+      .append("g")
+      /* assign unique id to each scaleBar */
+      .attr("id", function(d) {
+        return d;
+      })
       .append("rect")
         /* set rectangle attributes */
         .attr("y", function(d, i) {
@@ -36,10 +41,6 @@ function visualizeData() {
         .attr("width", scale_width)
         .attr("height", scale_height)
         .attr("fill", fill_color)
-        /* assign unique id to each scaleBar */
-        .attr("id", function(d) {
-          return d;
-        })
 
   /* create scaling for the scaleBar axes */
   var scaling_axis = d3.scaleLinear()
@@ -86,31 +87,76 @@ function visualizeData() {
 
     /* create array of values from each JSON property */
     var charNames = data.map(function(a) {return a.character;});
-    var results = data.map(function(a) {return (a.expression * 100) - 1;});
-    //console.log(charNames);
+    var expressionResults = data.map(function(a) {return (a.expression * 100) - 1;});
+    var awarenessResults = data.map(function(a) {return (a.awareness * 100) - 1;});
+    var hopeResults = data.map(function(a) {return (a.hope * 100) - 1;});
+    var embarrassmentResults = data.map(function(a) {return (a.embarrassment * 100) - 1;});
+    var empathyResults = data.map(function(a) {return (a.empathy * 100) - 1;});
+    var fearResults = data.map(function(a) {return (a.fear * 100) - 1;});
 
-    var scaling_ticks = d3.scaleOrdinal()
+    /* 1) EXPRESSION */
+    var scaling_expression = d3.scaleOrdinal()
       .domain(charNames)
-      /* expression scores in corresponding order */
-      .range(results);
+      .range(expressionResults); /* results for expression in corresponding order */
+    var tickMarks_expression = d3.axisTop(scaling_expression);
+    d3.select("#expression")
+      .append("g")
+        .attr("class", "tickMarks")
+        .call(tickMarks_expression);
 
-    var tickMarks = d3.axisTop(scaling_ticks);
+    /* 2) AWARENESS */
+    var scaling_awareness = d3.scaleOrdinal()
+      .domain(charNames)
+      .range(awarenessResults); /* results for awareness in corresponding order */
+    var tickMarks_awareness = d3.axisTop(scaling_awareness);
+    d3.select("#awareness")
+      .append("g")
+        .attr("class", "tickMarks")
+        .attr("transform", "translate(0," + dy + ")")
+        .call(tickMarks_awareness);
 
-    // d3.select("#expression")
-    //   .append("g")
-    //     .attr("class", "tickMarks")
-    //     .call(tickMarks);
+    /* 3) HOPE */
+    var scaling_hope = d3.scaleOrdinal()
+        .domain(charNames)
+        .range(hopeResults); /* results for hope in corresponding order */
+    var tickMarks_hope = d3.axisTop(scaling_hope);
+    d3.select("#hope")
+      .append("g")
+        .attr("class", "tickMarks")
+        .attr("transform", "translate(0," + (dy * 2) + ")")
+        .call(tickMarks_hope);
 
-    scaleBars.select("g")
-          .data(data_categories)
-          .enter()
-            .append("g")
-              .attr("class", "tickMarks")
-              /* moves each axis to the bottom of its scaleBar */
-              .attr("transform", function(d, i) {
-                return "translate(0," + (i * dy) + ")";
-              })
-              .call(tickMarks);
+    /* 4) EMBARRASSMENT */
+    var scaling_embarrassment = d3.scaleOrdinal()
+      .domain(charNames)
+      .range(embarrassmentResults); /* results for embarrassment in corresponding order */
+    var tickMarks_embarrassment = d3.axisTop(scaling_embarrassment);
+    d3.select("#embarrassment")
+      .append("g")
+        .attr("class", "tickMarks")
+        .attr("transform", "translate(0," + (dy * 3) + ")")
+        .call(tickMarks_embarrassment);
 
+    /* 5) EMPATHY */
+    var scaling_empathy = d3.scaleOrdinal()
+        .domain(charNames)
+        .range(empathyResults); /* results for empathy in corresponding order */
+    var tickMarks_empathy = d3.axisTop(scaling_empathy);
+    d3.select("#empathy")
+      .append("g")
+        .attr("class", "tickMarks")
+        .attr("transform", "translate(0," + (dy * 4) + ")")
+        .call(tickMarks_empathy);
+
+    /* 6) FEAR */
+    var scaling_fear = d3.scaleOrdinal()
+        .domain(charNames)
+        .range(fearResults); /* results for fear in corresponding order */
+    var tickMarks_fear = d3.axisTop(scaling_fear);
+    d3.select("#fear")
+      .append("g")
+        .attr("class", "tickMarks")
+        .attr("transform", "translate(0," + (dy * 5) + ")")
+        .call(tickMarks_fear);
   })
 }

e88e93a8c8ff6aba7c4e7a07fc11e770f9253b6b
marzipanmars <malam@vassar.edu>
2017-03-24 00:22:02 -0700

    add comments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 function visualizeData() {
+  /* set width and height of svg image */
   var canvas_width = 1000,
       canvas_height = 800;
 
+  /* scaling to adjust height of scaleBars if they exceed the range */
   var scaling = d3.scaleLinear()
-      .domain([0, 0])
       .range([0, canvas_height]);
 
+  /* create svg element via DOM manipulation */
   var canvas = d3.select(".svgContainer")
     .append("svg")
       .attr("width", canvas_width)
@@ -13,41 +15,55 @@ function visualizeData() {
     .append("g")
       .attr("transform", "translate(90, 90)")
 
-  var data_categories = ["expression", "awareness", "hope", "embarrassment", "empathy", "fear"], //"hunger", "joy", "memory", "morality", "joy", "pain", "personality", "attainment", "pleasure", "pride", "anger", "self-restraint", "thought"],
+/* set scaleBar labels, width, height, fill-color, and y-offset */
+  var data_categories = ["expression", "awareness", "hope", "embarrassment", "empathy", "fear", "hunger", "joy", "memory", "morality", "joy", "pain", "personality", "attainment", "pleasure", "pride", "anger", "self-restraint", "thought"],
       scale_width = 500,
       scale_height = 25,
       fill_color = "Gainsboro"
       dy = 120;
 
+/* create as many rectangles as there are elements in "data_categories" */
   var scaleBars = canvas.selectAll("rect")
+    /* binding data */
     .data(data_categories)
     .enter()
       .append("rect")
+        /* set rectangle attributes */
         .attr("y", function(d, i) {
+          /* "d" for data and "i" for the indices of the array */
           return i * dy;
         })
         .attr("width", scale_width)
         .attr("height", scale_height)
         .attr("fill", fill_color)
 
+  /* create scaling for the scaleBar axes */
   var scaling_axis = d3.scaleLinear()
+      /* set scale from 0-5 */
       .domain([0, 5])
+      /* pixel data */
       .range([0, scale_width - 1]);
 
+  /* create axis */
   var axis = d3.axisBottom()
+      /* set number of ticks on axis */
       .ticks(5)
+      /* pass scale as an argument */
       .scale(scaling_axis);
 
+  /* append axes to scaleBars via group "g" svg element */
   var scaleAxis = scaleBars.select("g")
     .data(data_categories)
     .enter()
       .append("g")
         .attr("class", "scaleAxis")
+        /* moves each axis to the bottom of its scaleBar */
         .attr("transform", function(d, i) {
           return "translate(0," + (i * dy + 25) + ")";
         })
         .call(axis);
 
+  /* create a label for each scaleBar */
   var categories = scaleBars.select("text")
     .data(data_categories)
     .enter()
@@ -61,7 +77,8 @@ function visualizeData() {
           return d;
         })
 
+  /* import JSON */
   d3.json("test_data.json", function(data) {
-    
+
   })
 }
